18ba3517d07c358d8bd0c6c4a2b836f6578eb1cc
NaokiHieu <huynhtanhieu.naoki@gmail.com>
2024-11-15 15:59:09 +0700

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+getgenv().Games = {
+  [2753915549] = "https://raw.githubusercontent.com/NaokiHieu/NaokiHub/refs/heads/main/Bf.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/NaokiHieu/NaokiHub/refs/heads/main/Bf.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/NaokiHieu/NaokiHub/refs/heads/main/Bf.lua", -- Blox Fruit Sea 3
+  [16732694052] = "https://raw.githubusercontent.com/NaokiHieu/NaokiHub/refs/heads/main/Fisch", -- Fisch
+}
